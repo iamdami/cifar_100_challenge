6ed20b6adbcc4870770ad8726216b3f364e0e551
Dami <onemoredami@gmail.com>
2024-10-08 19:21:34 +0900

    Create datasets.py

---
+++ resnet_cnn_project/datasets.py
@@ -0,0 +1,26 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(config):
+    # 데이터 전처리 및 augmentations 적용
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2470, 0.2435, 0.2616)),
+    ])
+    
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2470, 0.2435, 0.2616)),
+    ])
+    
+    train_dataset = datasets.CIFAR100(root=config['data']['root'], train=True, download=True, transform=transform_train)
+    test_dataset = datasets.CIFAR100(root=config['data']['root'], train=False, download=True, transform=transform_test)
+    
+    train_loader = DataLoader(train_dataset, batch_size=config['train']['batch_size'], shuffle=True, num_workers=config['data']['num_workers'])
+    test_loader = DataLoader(test_dataset, batch_size=config['train']['batch_size'], shuffle=False, num_workers=config['data']['num_workers'])
+    
+    return train_loader, test_loader
