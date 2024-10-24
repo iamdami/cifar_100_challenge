0f4421e441ef36b4b9928f1f6fc5ea4b34c11fc5
Dami <onemoredami@gmail.com>
2024-10-24 19:29:54 +0900

    Create datasets.py

---
+++ EfficientNet/datasets.py
@@ -0,0 +1,27 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(batch_size, num_workers=4):
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224),  # Resize to 224x224 for EfficientNet-B7
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+    ])
+
+
+
+    # Normalization for testing
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+    ])
+
+    train_dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+    test_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, test_loader
